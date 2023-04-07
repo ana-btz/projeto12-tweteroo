4877e22397810af5c27d5803b044e18912704ed9
ana_btz <silva.anabtz@gmail.com>
2023-04-07 08:25:03 -0300

    feat: implement sign-up route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Variáveis globais
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    const newUser = { username, avatar };
+
+    users.push(newUser);
+    res.send("OK");
+});
 
 app.listen(PORT, () => console.log(`Server rodando na porta: ${PORT}`));
