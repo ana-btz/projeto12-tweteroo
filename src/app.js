30033b14b0a42c249a61f2a2cc6a94c7812197ee
ana_btz <silva.anabtz@gmail.com>
2023-04-08 09:35:41 -0300

    feat: validate data received by POST request routes

--- src/app.js
+++ src/app.js
@@ -13,15 +13,16 @@ const tweets = [];
 
 app.get("/tweets", (req, res) => {
     if (tweets.length === 0) return res.send(tweets);
+
     const posts = [];
     let count = 0;
 
     for (let i = tweets.length - 1; i >= 0; i--) {
-        for (let j = 0; j < users.length; j++) {
-            if (users[j].username === tweets[i].username && count < 10) {
+        for (let item of users) {
+            if (item.username === tweets[i].username && count < 10) {
                 const post = {
-                    username: users[j].username,
-                    avatar: users[j].avatar,
+                    username: item.username,
+                    avatar: item.avatar,
                     tweet: tweets[i].tweet
                 };
 
@@ -36,6 +37,15 @@ app.get("/tweets", (req, res) => {
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
+
+    const invalid_username = !username || typeof (username) !== "string";
+    const invalid_avatar = !avatar || typeof (avatar) !== "string";
+
+    if (invalid_username || invalid_avatar) {
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return;
+    }
+
     const newUser = { username, avatar };
 
     users.push(newUser);
@@ -44,10 +54,20 @@ app.post("/sign-up", (req, res) => {
 
 app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body;
+
+    const invalid_username = !username || typeof (username) !== "string";
+    const invalid_tweet = !tweet || typeof (tweet) !== "string";
+
+    if (invalid_username || invalid_tweet) {
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return;
+    }
+
     const userExists = users.find(user => user.username === username);
 
     if (!userExists) {
-        return res.send("UNAUTHORIZED");
+        res.send("UNAUTHORIZED");
+        return;
     }
 
     const newTweet = { username, tweet };
